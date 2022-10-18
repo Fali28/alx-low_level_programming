cab8fe0f09fbb1075c820d437d44b50fe09c84dc
Fali28 <peacefaq@gmail.com>
2022-10-18 02:54:35 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Printing of -putchar
+ * Return: 0
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+}
