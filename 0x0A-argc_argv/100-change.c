3685e4d0187a4ef6495960c077b3aa9457892593
Fali28 <peacefaq@gmail.com>
2022-11-04 05:29:01 -0700

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,10 +22,10 @@ int main(int argc, char *argv[])
 	{
 		if (cents >= 25)
 			cents -= 25;
-		else if (cents >= 5)
-			cents -= 5;
 		else if (cents >= 10)
 			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
 		else if (cents >= 2)
 			cents -= 2;
 		else if (cents >= 1)
