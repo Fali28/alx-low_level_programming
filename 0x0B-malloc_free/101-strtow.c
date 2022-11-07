dd4f3a2e4e3e2706a66b0c32cbdbcbd32e29266e
Fali28 <peacefaq@gmail.com>
2022-11-07 10:30:35 -0800

    I will show you how great I am

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*strtow- a program that that splits a string into words
+*@str: string
+*Return: NULL if str == NULL or str == ""
+*/
+char **strtow(char *str)
+{
+	int x, y, z = 0, s, t, user = 0, bin;
+	char **letter;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	for (x = 0; str[x] != '\0'; x++)
+		if (str[x] != ' ' && (str[x + 1] == ' ' || str[x + 1] == '\0'))
+			user++;
+	if (user == 0)
+		return (NULL);
+	letter = malloc((user + 1) * sizeof(char *));
+	if (letter == NULL)
+	{
+		free(letter);
+		return (NULL);
+	}
+	for (x = 0; str[x] != '\0' && z < user; x++)
+	{
+		if (str[x] != ' ')
+		{
+			bin = 0;
+			for (y = x; str[y] != ' ' && str[y] != '\0'; y++)
+				bin++;
+			letter[z] = malloc((bin + 1) * sizeof(char));
+			if (letter[z] == NULL)
+			{
+				for (t = 0; t < z; t++)
+					free(letter[z]);
+					return (NULL);
+			}
+			for (s = 0; s < bin; s++, x++)
+				letter[z][s] = str[x];
+			letter[z][s] = '\0', z++;
+		}
+	}
+	letter[z] = NULL;
+	return (letter);
+}
