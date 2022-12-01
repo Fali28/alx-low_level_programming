bc4c1c066cb5b4ef24591a31e95895a184a8b847
Fali28 <peacefaq@gmail.com>
2022-12-01 13:16:27 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (x = 0; b[x] != '\0'; x++)
 	{
-		if (b[x] != '\0' && b[x] != '1')
+		if (b[x] != '0' && b[x] != '1')
 			return (0);
 	}
 	for (x = 0; b[x] != '\0'; x++)
