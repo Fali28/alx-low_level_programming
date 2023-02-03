4a397c4cc4c017b13c637e349cb6ed70532e82b7
Fali28 <peacaefaq@gmail.com>
2023-02-03 06:39:20 -0800

    6. >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,8 +1,9 @@
+
 #include "hash_tables.h"
 /**
- * hash_table_delete - a program that deletes a hash table
- * @ht: is the hash table
-**/
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_table_t *head = ht;
