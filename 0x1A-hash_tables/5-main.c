498e0999cb8c11644593600f6173dcb1db163465
Fali28 <peacaefaq@gmail.com>
2023-02-08 06:03:45 -0800

    7. ['Betty'] = 'Cool'

--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -2,12 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
+
 /**
  * main - check the code
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
+
 	hash_table_t *ht
 
 	ht = hash_table_create(1024);
